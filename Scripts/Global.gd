6253123008e92bed7a18ab106637c635d13e4f8b
laret1 <leoalberto1611@gmail.com>
2026-09-02 22:31:36 -0600

    sigo avanzando la tienda

--- Scripts/Global.gd
+++ Scripts/Global.gd
@@ -2,11 +2,23 @@ extends Node
 var minigames_done = 0 #track how many minigames done
 var lives = 5 #track how many lives left, also effecting garlic appearing
 var minigame_win = 2
-var skins_equipadas = {
-	"mario": "normal",
-	"undertale": "normal",
-	"flappy_bird": "normal"
+var catalogo_skins = {
+	"mario bros juego": {
+		"nombre": "mario bros fuego",
+		"precio": 100,
+		"desbloqueada": true,
+		
+	},
+	"sanes": {
+		"nombre": "sanes",
+		"precio": 150,
+		"desbloqueada": false,
+		
+	},
+	"flappy bird rojo": {
+		"nombre": "Flappy bird rojo",
+		"precio": 200,
+		"desbloqueada": false,
+		
+	}
 }
-
-
-var lista_todas_las_skins = ["espada_madera", "espada_fuego", "espada_hielo", "casco_hierro"]
